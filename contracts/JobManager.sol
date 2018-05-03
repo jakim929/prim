3adaa5ba4bd7878a5750bb525243dc8c3a6ab540
Jeffrey Wang <jeffwang@college.harvard.edu>
2018-05-03 17:14:34 -0400

    a version almost adjusted but cant pass structs in functions

--- contracts/JobManager.sol
+++ contracts/JobManager.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.17;
 
-import "./ImageLabel.sol";
+/* import { ImageLabel as IL } from "./ImageLabel.sol"; */
 
 
 contract JobManager {
@@ -8,36 +8,208 @@ contract JobManager {
     struct Labeller {
         bool set;
         uint16 streak;
-        mapping(address => bool) jobs;
+        mapping(uint64 => bool) jobs;
         uint16 latestJob;
     }
 
-    uint16 public numJobs = 0;
-    uint16 public currentJob = 0;
+    struct Job {
+        uint8 gameType;
+        string imageLink;
+        string query;
+        uint bounty;
+        uint16 index;
+        address[] agents;
+        uint8 numClaimers;
+        uint8 numAnswered;
+        mapping (address => int8) addrToAns;
+        mapping (address => bool) settled;
+        mapping (address => uint) amountEarned;
+        mapping (address => uint) pendingWithdrawals;
+    }
 
-    address[] public jobs;
-
-    mapping (address => Labeller) public labellers;
+    uint16 numJobs = 0;
+    uint16 currentJob = 0;
+    Job[] jobs;
+    mapping (address => Labeller) labellers;
     address public owner;
 
     modifier sufficientFunds(uint bounty, uint available) { require(bounty < available); _; }
     modifier notFree(uint bounty) { require(bounty > 0); _;}
-    modifier availableJob(ImageLabel job) { require(job.availableJob()); _; }
-    modifier claimed(ImageLabel job) { require(job.numClaimers() == job.gameType()); _; }
-    modifier notSettled(ImageLabel job) {require(!(job.isSettled())); _; }
+    modifier availableJob(Job job) { require(job.numClaimers < job.gameType); _; }
+    modifier claimed(Job job) { require(job.numClaimers == job.gameType); _; }
+    /* modifier notSettled(Job job) {require(!(job.settled[msg.sender])); _; } */
     modifier workLeftFor(address addr) {require(!(labellers[addr].latestJob == numJobs)); _; }
+    modifier hasBeenAssigned(Job job, address addr){
+        require(labellers[addr].jobs[job.index]);
+         _;
+    }
+    modifier answered(Job job) {require(job.numAnswered == job.gameType); _;}
 
     constructor() public payable
     {
         owner = msg.sender;
     }
 
-    function indexToAddr(uint16 i) public returns(address){
-        return jobs[i];
+
+    function claimSpecificJob(uint16 jobIndex, address person)
+        public
+        returns(bool)
+    {
+        Job storage job = jobs[jobIndex];
+
+        upsertLabeller(person);
+        if(!labellers[person].set){
+            return false;
+        }
+        giveJob(person, job);
+        if(!labellers[person].jobs[job.index]){
+            return false;
+        }
+        return true;
     }
 
-    function jobAssigned(address job, address addr) public returns (bool){
-        return labellers[addr].jobs[job];
+    function answerJob(Job job, int8 answer)
+        public
+        hasBeenAssigned(job, msg.sender)
+        returns (int8)
+    {
+        job.addrToAns[msg.sender] = answer;
+        job.numAnswered += 1;
+        return answer;
+    }
+
+    function claimJob(Job job)
+        public
+        returns (bool)
+    {
+        if(!(claimSpecificJob(msg.sender, job))){
+            return false;
+        }
+
+        job.agents.push(msg.sender);
+        job.numClaimers += 1;
+
+        if (job.numClaimers == job.gameType){
+            markClaimed(job);
+        }
+
+        return true;
+    }
+
+    function claimAnswerJob(Job job, int8 answer)
+        public
+        returns(int8)
+    {
+        claimJob(job);
+        return answerJob(job, answer);
+    }
+
+    function claimAnswerSettleJob(Job job, int8 answer)
+        public
+        returns(bool)
+    {
+        claimAnswerJob(job, answer);
+        return settle(job);
+    }
+
+    function positiveWithdrawal(Job job, address addr) public returns (bool){
+        return (job.pendingWithdrawals[addr] > 0);
+    }
+
+    function esp(Job job)
+        /* notSettled(job) */
+        answered(job)
+        private
+        returns (bool)
+    {
+        require(!(job.settled[msg.sender]));
+
+        if (job.addrToAns[job.agents[0]] == job.addrToAns[job.agents[1]]){
+            job.pendingWithdrawals[job.agents[0]] = job.bounty/2;
+            job.pendingWithdrawals[job.agents[1]] = job.bounty/2;
+        }
+        return (job.addrToAns[job.agents[0]] == job.addrToAns[job.agents[1]]);
+    }
+
+    function majority(Job job)
+        /* notSettled(job) */
+        answered(job)
+        private
+        returns (bool)
+    {
+        require(!(job.settled[msg.sender]));
+
+        int8 popularAnswer;
+        mapping (int8 => uint8) ansToVotes;
+        for (uint8 i = 0; i < job.gameType; i++){
+            int8 answer = job.addrToAns[job.agents[i]];
+            ansToVotes[answer] += 1;
+            if (ansToVotes[answer] > ansToVotes[popularAnswer]){
+                popularAnswer = answer;
+            }
+        }
+        if (ansToVotes[popularAnswer] < job.gameType - 1){
+            return false;
+        }
+        else {
+            uint8 split = ansToVotes[popularAnswer];
+            for (i = 0; i < job.gameType; i++){
+                if (job.addrToAns[job.agents[i]] == popularAnswer){
+                    job.pendingWithdrawals[job.agents[i]] = job.bounty / uint(split);
+                }
+            }
+            return (job.addrToAns[msg.sender] == popularAnswer);
+        }
+    }
+
+    function consensus(Job job)
+        /* notSettled(job) */
+        answered(job)
+        private
+        returns (bool)
+    {
+        require(!(job.settled[msg.sender]));
+        return true;
+    }
+
+    function settle(Job job)
+        /* notSettled(job) */
+        answered(job)
+        public
+        returns (bool)
+    {
+        require(!(job.settled[msg.sender]));
+        bool isPaid;
+    	if (job.gameType == 2){
+            isPaid = esp(job);
+        }
+      	else {
+            isPaid = majority(job);
+        }
+        /* else if (gameType > 3){
+            isPaid = consensus();
+        } */
+
+        uint amount = job.pendingWithdrawals[msg.sender];
+        if (isPaid){
+            updateStreak(job, msg.sender);
+            job.amountEarned[msg.sender] = job.pendingWithdrawals[msg.sender];
+            job.pendingWithdrawals[msg.sender] = 0;
+            if (!msg.sender.send(amount)){
+                // No need to call throw here, just reset the amount owing
+                job.pendingWithdrawals[msg.sender] = amount;
+                return false;
+            }
+        }
+        else{
+            updateStreak(job, msg.sender);
+        }
+        job.settled[msg.sender] = true;
+        return true;
+    }
+
+    function getEarnings(Job job) public returns (uint){
+        return job.amountEarned[msg.sender];
     }
 
     function getBalance() public view returns (uint)
@@ -45,7 +217,7 @@ contract JobManager {
         return address(this).balance;
     }
 
-    // function addJob that creates a job and adds it to the mapping
+    // function addJob that creates a job and adds it
     function addJob(
         string imageLink,
         string query,
@@ -54,13 +226,17 @@ contract JobManager {
         sufficientFunds(bounty, address(this).balance)
         notFree(bounty)
         public
-        returns (address)
+        returns (uint16)
     {
-        ImageLabel newJob = (new ImageLabel).value(bounty)(imageLink, query, gameType, this, numJobs);
-        jobs.push(newJob);
+        jobs.push(Job({
+            gameType: gameType,
+            imageLink: imageLink,
+            query: query,
+            bounty: bounty,
+            index: numJobs
+        }));
         numJobs += 1;
-
-        return newJob;
+        return numJobs;
     }
 
     function upsertLabeller(address addr) public {
@@ -69,37 +245,20 @@ contract JobManager {
         }
     }
 
-    function giveJob(address addr, address job, uint16 index)
-        /* availableJob(job) */
+    function giveJob(address addr, Job job)
+        availableJob(job)
         public
     {
-
-        labellers[addr].jobs[job] = true;
-        labellers[addr].latestJob = index;
-    }
-
-    function claimSpecificJob(address person, address job, uint16 index)
-        public
-        returns(bool)
-    {
-        upsertLabeller(person);
-        if(!labellers[person].set){
-            return false;
-        }
-        giveJob(person, job, index);
-        if(!labellers[person].jobs[job]){
-            return false;
-        }
-        return true;
-
+        labellers[addr].jobs[job.index] = true;
+        labellers[addr].latestJob = job.index;
     }
 
     function setCurrentJob(uint16 val) public {
         currentJob = val;
     }
 
-    function markClaimed(ImageLabel job)
-        /* claimed(job) */
+    function markClaimed(Job job)
+        claimed(job)
         public
     {
         currentJob += 1;
@@ -109,10 +268,12 @@ contract JobManager {
         return labellers[addr].streak;
     }
 
-    function updateStreak(ImageLabel job, address addr)
+    function updateStreak(Job job, address addr)
         /* notSettled(job) */
         public
     {
+        require(!(job.settled[msg.sender]));
+
         if (job.positiveWithdrawal(addr)){
             labellers[addr].streak += 1;
         }
@@ -122,11 +283,10 @@ contract JobManager {
     }
 
     function getJob()
-        /* workLeftFor(msg.sender) */
+        workLeftFor(msg.sender)
         public
         returns (address)
     {
-        /* return jobs[currentJob]; */
         // Returns the sender's address if out of jobs.
         uint16 labellerLatestJob = labellers[msg.sender].latestJob;
         if(labellerLatestJob == numJobs - 1){
@@ -136,12 +296,9 @@ contract JobManager {
         if(labellerLatestJob > currentJob){
             jobSearch = labellerLatestJob;
         }
-        while(jobAssigned(ImageLabel(jobs[jobSearch]), msg.sender)){
+        while (labellers[msg.sender].jobs[jobSearch]){
             jobSearch += 1;
-
         }
-
-
         return jobs[jobSearch];
     }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,34 +1,31 @@
-var ImageLabel = artifacts.require("ImageLabel.sol");
 var JobManager = artifacts.require("JobManager.sol");
 
 var manager;
 
 const jobList = [
-    ["https://tinyurl.com/ya4ukkc3" ,"Is there a hotdog in this image?", 2, 100000000000000],
-    ["http://images.wisegeek.com/hot-dog-with-mustard.jpg", "Is this a hotdog?", 2, 100000000000000],
-    ["https://tinyurl.com/ybelqvn5", "Is this Professor Kung?", 2, 100000000000000],
-    ["https://s3.amazonaws.com/marcuscomiterfiles/img/marcus.jpg", "Is this a CS144r TF?", 2, 100000000000000],
-    ["https://tinyurl.com/y86wsg2r", "Is this a blockchain?", 2, 100000000000000]
+    ["https://tinyurl.com/ya4ukkc3" ,"Is there a hotdog in this image?", 2, Number("1E14")],
+    ["https://tinyurl.com/ljnspby", "Is this a hotdog?", 2, Number("1E14")],
+    ["https://tinyurl.com/ybelqvn5", "Is this Professor Kung?", 2, Number("1E14")],
+    ["https://tinyurl.com/ydgadcfk", "Is this a CS144r TF?", 2, Number("1E14")],
+    ["https://tinyurl.com/y86wsg2r", "Is this a blockchain?", 2, Number("1E14")]
 ];
 
-var jobAddresses = []
+// var jobAddresses = [];
 
 module.exports = function(deployer, network, accounts) {
     deployer.deploy(
         JobManager,
-        {gas: 50000000, value: 1000000000000000}
+        {gas: 5e7, value: 1e15}
     ).then(() => {
         return JobManager.deployed();
-    }).then(async  (instance) => {
+    }).then(async (instance) => {
         manager = instance;
         for (let i = 0; i < jobList.length; i++){
             let addr = await manager.addJob(...jobList[i]);
         }
-        for (let i = 0; i < jobList.length; i++){
-            let addr = await manager.jobs.call(i);
-            jobAddresses.push(addr);
-        }
-        console.log(jobAddresses)
-
+        // for (let i = 0; i < jobList.length; i++){
+        //     let addr = await manager.jobs.call(i);
+        //     jobAddresses.push(addr);
+        // }
     })
 }
