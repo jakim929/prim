cef49c4fb4ad5507f04d47d225548546c288cc3e
Jeffrey Wang <jeffwang@college.harvard.edu>
2018-05-05 10:45:23 -0400

    hmm

--- truffle.js
+++ truffle.js
@@ -16,7 +16,7 @@ module.exports = {
             provider: function() {
                 return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/ObzHnsqZcHsvUb5McnGH")
             },
-            network_id: 3,
+            network_id: *,
             gas: 7005938,
         }
     }
